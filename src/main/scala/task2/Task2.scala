d39e2cfe8e47ea440d0d4b0f39acaf5235e03f3e
oleg <oleg.sharnikov@outlook.com>
2019-08-17 22:03:57 +0300

    Extend config

--- src/main/scala/task2/Task2.scala
+++ src/main/scala/task2/Task2.scala
@@ -14,30 +14,32 @@ import scala.concurrent.Future
 import scala.concurrent.duration.FiniteDuration
 import scala.util.Success
 
-
 //TODO generator
-//@TODO test
-//@TODO config
+//TODO test
+//TODO sort ????
 object Task2 extends Settings(ConfigFactory.parseFile(new File("src/main/resources/app.conf")))
   with App
   with DataExtractor
   with DataGenerator {
 
-  def aggregateFilmsInfo(path: String) = {
-    getDataSource(path, extractor.frameLength, extractor.delimiter)
-      .map(_.split(',').toList)
-      .groupedWithin(10, FiniteDuration(1, TimeUnit.SECONDS))
-      .mapAsync(4)(sample => Future { sample.groupMapReduce(_.head)(_.tail.toSet)(_ |+| _) })
-      .runFold(Map.empty[String, Set[String]])(_ |+| _)
+  val infoGetter = new PartialFunction[String, List[String]] {
+    override def isDefinedAt(value: String): Boolean = value.split(',').nonEmpty
+    override def apply(value: String): List[String] = value.split(',').toList
   }
 
-   val result = aggregateFilmsInfo(paths.filmsFilePath)
+  def aggregateFilmsInfo(path: String) =
+    getDataSource(path, extractor.frameLength, extractor.delimiter)
+    .collect(infoGetter)
+    .groupedWithin(films.chunkSize, films.chunkTime)
+    .mapAsync(films.streamsParallelism)(sample => Future { sample.groupMapReduce(_.head)(_.tail.toSet)(_ |+| _) })
+    .runFold(Map.empty[String, Set[String]])(_ |+| _)
+
+   val result = aggregateFilmsInfo(paths.filmsFilePath).map(_.toList.sortBy(_._1))
 
   result.onComplete {
     case Success(value) =>
       println(value.foreach(println))
       system.terminate()
   }
-
 
 }
--- src/main/scala/utils/Settings.scala
+++ src/main/scala/utils/Settings.scala
@@ -2,10 +2,14 @@ package utils
 
 import com.typesafe.config.Config
 import net.ceedubs.ficus.Ficus._
+import Settings._
+
+import scala.concurrent.duration.{Duration, FiniteDuration}
 
 abstract class Settings(config: Config) {
   def extractor: Extractor = new ExtractorImpl(config.as[Config]("extractor"))
   def paths: Paths = new PathsImpl(config.as[Config]("paths"))
+  def films: Films = new FilmsImpl(config.as[Config]("films"))
 }
 
 trait Extractor {
@@ -26,4 +30,24 @@ trait Paths {
 class PathsImpl(config: Config) extends Paths {
   override def numbersFilePath: String = config.getString("numbers.file")
   override def filmsFilePath: String = config.getString("films.file")
+}
+
+trait Films {
+  def chunkSize: Int
+  def chunkTime: FiniteDuration
+  def streamsParallelism: Int
+}
+
+class FilmsImpl(config: Config) extends Films {
+  override def chunkSize: Int = config.getInt("chunk.size")
+  override def chunkTime: FiniteDuration = config.getDuration("chunk.time").toScalaDuration()
+  override def streamsParallelism: Int = config.getInt("streams.parallelism")
+}
+
+
+
+object Settings {
+  implicit class duration(duration: java.time.Duration) {
+    def toScalaDuration(): FiniteDuration = Duration.fromNanos(duration.toNanos)
+  }
 }
